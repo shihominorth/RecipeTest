6a4209140c92079ca3b130e506fa070a5118b0b2
Shihomi Kitajima <shihomi0104@icloud.com>
2019-11-26 03:33:07 -0800

    change cell's color

--- RecipeTest/profieTableViewCell.swift
+++ RecipeTest/profieTableViewCell.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class profieTableViewCell: UITableViewCell {
+    
+    let RecipeListCreator = recipeListCreator()
 
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var creatorNameLabel: UILabel!
     @IBOutlet weak var numRecipesLabel: UILabel!
     @IBOutlet weak var numFollwersLabel: UILabel!
     
+    var numFollwers = 0
+    lazy var numRecipes = RecipeListCreator.creatorRecipeLists.count
+    
+    
+    @IBAction func followersAddButton(_ sender: Any) {
+        numFollwers += 1
+    }
+    
     
     // this is called when it is initialized.
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        creatorNameLabel.text = "Lay FangYi"
+        numFollwersLabel.text = "\(numFollwers) follwers"
+        numRecipesLabel.text = "\(numRecipes) recipes"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
